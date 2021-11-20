89221e26ff3aca79281e68547f1a548010c56486
Gibson Silali <gibsonsilali@gmail.com>
2021-11-20 12:40:17 +0300

    removed console logs

--- src/app/services/auth/authentication.guard.ts
+++ src/app/services/auth/authentication.guard.ts
@@ -1,19 +1,19 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
 import {AuthService} from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
-  constructor(private authService : AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router) {
 
   }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    console.log('hello boyzzzz')
     if (this.authService.isAuthenticated()) {
       return true;
     } else {
